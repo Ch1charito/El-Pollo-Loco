ff26a9d605890280a108e27d5302324edf6edeee
Ch1charito <r.buschmann.work@gmail.com>
2025-05-15 16:54:18 +0200

    added some classes

---
+++ models/movable-object.class.js
@@ -0,0 +1,10 @@
+class MovableObject{                            // eine Schablone mit der wir sagen welche Felder drin sein sollen
+    x = 120;
+    y = 400;
+    img;
+
+    moveRight() {
+        console.log('moving right');
+        
+    }
+}
