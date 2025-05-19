32e602f2939a3a83025c15f4f88b593d59b6e8c4
Ch1charito <r.buschmann.work@gmail.com>
2025-05-19 16:36:48 +0200

    added function to render trough arrays of objects

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -1,8 +1,12 @@
 class Cloud extends MovableObject{
+    y = 20;                                                                             // da nicht dynamisch soll standard mäßig sein --> sauberer als wenn es direkt im constructor drinnen liegt
+    width = 500;
+    height = 250;
 
     constructor(){                              
         super().loadImage('img/5_background/layers/4_clouds/1.png');                    // mit super greifen wir auf die function aus der übergeordneten class movable objects zu
-        this.x = 200 + Math.random() * 500;     // mit math.random erechne ich einen rdnm wert ziwschen 0 und 1. Weil man 1 pixel aber nicht sieht mulitplieziere ich den wert mit 500 und kriege einen Wert on 0 bis 500, das ganze addiere ich ja auf die 200 rauf--> so starteter bei 200 und geht bis 700
+        this.x = Math.random() * 500;      
+        
     }
 
 }
--- models/world.class.js
+++ models/world.class.js
@@ -8,6 +8,9 @@ class World{
     clouds = [
         new Cloud()
     ];
+    backgroundObjects = [
+        new BackgroundObject('img/5_background/layers/3_third_layer/1.png')
+    ];
     canvas;
     ctx;
 
@@ -17,20 +20,31 @@ class World{
         this.draw();                                    // beim erstelen der neuen Welt wird der constructor aufgerufen und somit auch draw mit der wir pepe darstellen können beim erstellen einer welt -> init function
     }
 
-
+    // #region objects hinzufügen
     draw(){
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)                                                              // wir clearen den inhalt im canvas weil das bild sonst öfter gezeichnet wird bei jedem verschieben 
-        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height)      // mit this.character.img greifen wir auf des Bild unseres Characters zu ; pos1 = img, pos2= x position in canvas, pos3= yposition in canvas, pos4= breite und pos5=höhe
-        this.enemies.forEach(enemies => {                                                                                            // eine schleife das durch jedes objekt in enemies wiederholt
-            this.ctx.drawImage(enemies.img, enemies.x, enemies.y, enemies.width, enemies.height)                                     // wir führen die drawImage methode für jedes element aus enemies aus
-        });
-        this.clouds.forEach(clouds => {                                                                                            // eine schleife das durch jedes objekt in clouds wiederholt
-            this.ctx.drawImage(clouds.img, clouds.x, clouds.y, clouds.width, clouds.height)                                     // wir führen die drawImage methode für jedes element aus clouds aus
-        });
+        /* this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height)      // mit this.character.img greifen wir auf des Bild unseres Characters zu ; pos1 = img, pos2= x position in canvas, pos3= yposition in canvas, pos4= breite und pos5=höhe */
+        this.addToMap(this.character);
+        this.addObejctsToMap(this.backgroundObjects);
+        this.addObejctsToMap(this.enemies);
+        this.addObejctsToMap(this.clouds);
 
         self = this;                                    // draw wird immer wieder aufgerufen
         requestAnimationFrame(function(){
             self.draw();
         });
     }
+
+    addObejctsToMap(objects){                                                                                                       // eine function zum vereinfachen und hinzufügen von objekten
+        objects.forEach(o => {
+            this.addToMap(o);
+        });
+    }
+
+    addToMap(mo){
+        this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);                                                                // wir erstellen eine function um diese immer auszuführen wenn wir etwas darstellen wollen             
+    }
+    // #endregion
+
+
 }
