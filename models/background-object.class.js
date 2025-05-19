b7fcbeaa1f16fe6e4eedf3de49bab66b4ac0d60a
Ch1charito <r.buschmann.work@gmail.com>
2025-05-19 16:43:53 +0200

    added background

--- models/background-object.class.js
+++ models/background-object.class.js
@@ -1,9 +1,12 @@
 class BackgroundObject extends MovableObject{
 
+    width = 720;                                // wir vergeben für den hintergrund eine standard höhe und eine standard breite
+    height = 400;
 
-
-    constructor(imagePath){
+    constructor(imagePath, x, y){
         super().loadImage(imagePath);
+        this.y = y;
+        this.x = x;
     }
 
 }
