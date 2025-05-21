13448de1547826035ba964a11e236a800fa171a0
Ch1charito <r.buschmann.work@gmail.com>
2025-05-21 12:36:50 +0200

    added multiple layers of the map

--- models/world.class.js
+++ models/world.class.js
@@ -10,10 +10,28 @@ class World{
         new Cloud()
     ];
     backgroundObjects = [
+        new BackgroundObject ('img/5_background/layers/air.png', -719),                              
+        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', -719),
+        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', -719),
+        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', -719),
+
         new BackgroundObject ('img/5_background/layers/air.png', 0),
         new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0),
         new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 0),
         new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0),
+        new BackgroundObject ('img/5_background/layers/air.png', 719),                              // wir brauchen 719 und nicht 720 weil sonst ein strich sitchbar wird --> das kommt weil wir von 0 zählen und nicht von 1
+        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 719),
+        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 719),
+        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 719),
+
+        new BackgroundObject ('img/5_background/layers/air.png', 719 * 2),
+        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 719 * 2),
+        new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 719 * 2),
+        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 719 * 2),
+        new BackgroundObject ('img/5_background/layers/air.png', 719 * 3),                              
+        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 719 * 3),
+        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 719 * 3),
+        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 719 * 3),
         
     ];
     canvas;
